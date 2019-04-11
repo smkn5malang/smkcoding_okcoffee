7eec75c2eb94cfe513d39dddc97701c62c01ea7c
fadillahyulias <fadillahyulia2002@gmail.com>
2019-04-11 14:05:23 -0700

    First

---
+++ app/src/main/java/com/example/okcoffee/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.okcoffee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn,btnSignUp;
+    TextView txtLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+
+        txtLogin = (TextView)findViewById(R.id.txtLogin);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
